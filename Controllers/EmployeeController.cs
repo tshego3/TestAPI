16d12800b189eb7f5847dea1426e565397b03e5c
Tshegofatso Motswagae <tshegofatsomotswagae@hotmail.com>
2021-03-19 06:09:09 +0200

    Update

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -14,39 +14,68 @@ namespace TestAPI.Controllers
     {
         private IT_CompanyDBDataContext db = new IT_CompanyDBDataContext();
 
+        public Models.Employee[] Employees
+        {
+            get
+            {
+                return (from e in db.Employees
+                        select new Models.Employee()
+                        {
+                            EmployeeID = e.employeeID,
+                            TbFirstName = e.tbFirstName,
+                            TbSurname = e.tbSurname,
+                            TbTellNo = e.tbTellNo,
+                            TbEmail = e.tbEmail,
+                            UserID = e.UserID
+                        }).ToArray();
+            }
+        }
+
         // GET: api/Employee
         public IEnumerable<Models.Employee> Get()
         {
             //return new string[] { "value1", "value2" };
 
-            List<Models.Employee> EmployeeData = new List<Models.Employee>();
-            Models.Employee employee = new Models.Employee();
+            //List<Models.Employee> EmployeeData = new List<Models.Employee>();
+            //Array[] EmployeeData
+            //Models.Employee employee = new Models.Employee();
 
-            var GetEmployeeData = from e in db.Employees
-                                  select new
-                                  {
-                                      EmployeeID = e.employeeID,
-                                      TbFirstName = e.tbFirstName,
-                                      TbSurname = e.tbSurname,
-                                      TbTellNo = e.tbTellNo,
-                                      TbEmail =e.tbEmail,
-                                      UserID = e.UserID
-                                  };
+            //var GetEmployeeData = from e in db.Employees
+            //                      orderby e.employeeID descending
+            //                      select new
+            //                      {
+            //                          EmployeeID = e.employeeID,
+            //                          TbFirstName = e.tbFirstName,
+            //                          TbSurname = e.tbSurname,
+            //                          TbTellNo = e.tbTellNo,
+            //                          TbEmail =e.tbEmail,
+            //                          UserID = e.UserID
+            //                      };
 
-            foreach (var ge in GetEmployeeData)
-            {
-                employee.EmployeeID = ge.EmployeeID;
-                employee.TbFirstName = ge.TbFirstName;
-                employee.TbSurname = ge.TbSurname;
-                employee.TbTellNo = ge.TbTellNo;
-                employee.TbEmail = ge.TbEmail;
-                employee.UserID = ge.UserID;
-                EmployeeData.Add(employee);
-            }
+            //foreach (var ge in GetEmployeeData)
+            //{
+            //    employee.EmployeeID = ge.EmployeeID;
+            //    employee.TbFirstName = ge.TbFirstName;
+            //    employee.TbSurname = ge.TbSurname;
+            //    employee.TbTellNo = ge.TbTellNo;
+            //    employee.TbEmail = ge.TbEmail;
+            //    employee.UserID = ge.UserID;
+            //    EmployeeData.Add(employee);
+            //}
 
-            return EmployeeData;
+            //return EmployeeData;
+
+            return Employees;
         }
 
+        // GET: api/Employee/5
+        [Route("api/Employee/Search/{FirstName}")]
+        [HttpGet]
+        public IEnumerable<Models.Employee> Get(string FirstName)
+        {
+            return Employees.Where(e => e.TbFirstName.Contains(FirstName));
+        }
+        
         // GET: api/Employee/5
         public string Get(int id)
         {
